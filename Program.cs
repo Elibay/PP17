5e3270e051cd5c9ed57ee396cb18ed280a0e6286
Elibay <elibay98@gmail.com>
2017-01-23 21:50:29 +0600

    Week 1 1-3

--- Program.cs
+++ Program.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿
+using System;
 
 namespace Application
 {
 	class MainClass
 	{
+		// Function check we must check number is prime or not
+		static bool check(int x)
+		{
+			if (x == 1)
+				return false;
+			for (int i = 2; i * i <= x; ++i)
+				if (x % i == 0)
+					return false;
+			return true;
+		}
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int n = args.Length;
+			for (int i = 0; i < n; ++i)
+			{
+				// Convert to int
+				int p = int.Parse(args[i]);
+				if (check(p))
+					// Print this number
+					Console.WriteLine(p);
+			}
 		}
 	}
 }
