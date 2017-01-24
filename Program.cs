14e548697b7a2a0a80034086be0e741025501227
Elibay <elibay98@gmail.com>
2017-01-24 21:25:13 +0600

    Week 2 3

--- Program.cs
+++ Program.cs
@@ -4,20 +4,29 @@ namespace Student
 {
 	class MainClass
 	{
+		static bool check(int x)
+		{
+			if (x == 1)
+				return false;
+			for (int i = 2; i * i <= x; ++i)
+				if (x % i == 0)
+					return false;
+			return true;
+		}
 		static void MaxMin()
 		{
 			StreamReader sr = new StreamReader(@"/Users/elibay/Documents/input.txt");
+			StreamWriter sw = new StreamWriter(@"/Users/elibay/Documents/output.txt");
 			string[] arr = sr.ReadLine().Split();
-			int x = 9999, y = 0;
+			int x = 999999;
 			foreach (string s in arr)
 			{
 				int p = int.Parse(s);
-				if (p < x)
+				if (p < x && check (p))
 					x = p;
-				if (p > y)
-					y = p;
 			}
-			Console.WriteLine(x + " " + y);
+			sw.WriteLine(x);
+			sw.Close();
 			sr.Close();
 		}
 		static void Main(string[] args)
