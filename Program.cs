52f1b4d8fdc1f28ea4a3cd30fb053d81934af8d1
Elibay <elibay98@gmail.com>
2017-01-26 12:27:54 +0600

    Week 2 1 part 2

--- Program.cs
+++ Program.cs
@@ -10,6 +10,41 @@ namespace Student
 				Console.Write("-");
 			Console.Write("> ");
 		}
+		static void Get(string path)
+		{
+			Console.WriteLine("gg");
+			int[] lvl = new int [10000];
+			string[] a = new string[1000];
+
+			int l = 0, r = 1;
+			lvl[0] = 0;
+			a[0] = path;
+			while (l < r)
+			{
+				++ l;
+				if (lvl[l] > 3)
+					continue;
+				DirectoryInfo directory = new DirectoryInfo(a[l - 1]);
+				FileInfo[] files = directory.GetFiles();
+				DirectoryInfo[] directories = directory.GetDirectories();
+				foreach (FileInfo file in files)
+				{
+					emptySpace(lvl[l - 1]);
+					Console.WriteLine(file.Name);
+				}
+				foreach (DirectoryInfo dInfo in directories)
+				{
+					emptySpace(lvl[l - 1]);
+					Console.WriteLine(dInfo.Name);
+					if (r <= 1000)
+					{
+						a[r] = (dInfo.FullName);
+						lvl[r] = lvl[l - 1] + 1;
+						++r;
+					}
+				}
+			}
+		}
 		static void Rec(string path, int level)
 		{
 			if (level > 3)
@@ -31,7 +66,8 @@ namespace Student
 		}
 		static void Main(string[] args)
 		{
-			Rec (@"/Users/elibay/Projects", 0);
+			Get(@"/Users/elibay/Projects");
+			//Rec (@"/Users/elibay/Projects", 0);
 		}
 	}
 }
